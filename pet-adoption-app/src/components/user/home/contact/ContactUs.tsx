c424c4c4bd6a678127afa56c27d6b85526e74303
pujakunche <kunchepuja1994@gmail.com>
2024-12-22 01:50:17 -0500

    added some elements to contact us webpage

--- pet-adoption-app/src/components/user/home/contact/ContactUs.tsx
+++ pet-adoption-app/src/components/user/home/contact/ContactUs.tsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import Navbar from '../../../navbar/Navbar'
+import { Container } from 'react-bootstrap'
 
 function ContactUs() {
   return (
-    <div>ContactUs</div>
+    <div className='root'>
+        <Navbar/>
+
+        <Container>
+            <div className='contactgroup'>
+                <div className='contacttitlehead'>
+                    <h1 className='contacttitle'> Contact US</h1>
+                </div>
+                <p className='contactparagraph'> Please reach out to us to start your new life with your favorite companion!</p>
+                <div className='contacttitlehead2'>
+                    <h1 className='contacttitle'> Our Location</h1>
+                </div>
+                <p className='contactparagraph'>New Jersey Princeton Lawerence Township</p>
+                <div className='contacttitlehead3'>
+                    <h1 className='contacttitle'> Call Us To Book An Appointment</h1>
+                </div>
+                <p className='contactparagraph'>478 - 123 - 4567</p>
+            </div>
+        </Container>
+        
+    </div>
   )
 }
 
