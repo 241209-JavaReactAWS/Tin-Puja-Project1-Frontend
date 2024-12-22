430a9c756fcfd955dccf7806206e170bdd34e611
pujakunche <kunchepuja1994@gmail.com>
2024-12-22 12:17:13 -0500

    added navbar to pets

--- pet-adoption-app/src/components/user/home/pets/Pets.tsx
+++ pet-adoption-app/src/components/user/home/pets/Pets.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { Container, Table } from 'react-bootstrap';
+import Navbar from '../../../navbar/Navbar';
 
 function Pets() {
 
@@ -15,127 +16,43 @@ function Pets() {
     }, [])
     
   return (
-    <>
-    <Container>
-        <h1>Pets</h1>
+    <div className='root'>
+        <Navbar/>
+        <Container>
+            <h1>Pets</h1>
+            <div>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>Select</th>
+                            <th>Picture</th>
+                            <th>Breed</th>
+                            <th>Name</th>
+                            <th>Age</th>
+                            <th>Condition</th>
+                            <th>Gender</th>
+                            <th>Available</th>
 
- <div>
-    <Table striped bordered hover>
-        <thead>
-            <tr>
-                <th>Select</th>
-                <th>Picture</th>
-                <th>Breed</th>
-                <th>Name</th>
-                <th>Age</th>
-                <th>Condition</th>
-                <th>Gender</th>
-                <th>Available</th>
-
-            </tr>
-                </thead>
-                <tbody>
-        {allPets.map((item) => (
-            <tr key={item.petId}>
-            <td><input type="checkbox"/></td>
-            <td><img className="photos" src="/photos/golden.jpg" alt="Golden Retriever"/></td>
-            <td>{item.breed}</td>
-            <td>{item.name}</td>
-            <td>{item.age}</td>
-            <td>{item.petCondition}</td>
-            <td>{item.gender}</td>
-            <td>{item.status}</td>
-
-
-        </tr>
-
-
-        //   <p key={item.petId}>{item.name}</p>
-
-        ))}
-            </tbody>
-        </Table>
-      </div>
-
-
-      {/* <div>
-          <h1>Fetching all pets stuff -- temp</h1>
-
-          
-          {
-              allPets.map( (pet) => {
-                  return (
-                      <div key={pet.petId}>
-                        <p>{pet.name}</p>
-                      </div>
-                  )
-              }
-
-              )
-          }
-          
-      </div> */}
-
-      {/* <div>
-        {data && (
-          <ul>
-            {data.map((item) => (
-              <li key={item.id}>{item.name}</li>
-            ))}
-          </ul>
-        )}
-      </div> */}
-
-        {/* <section className="tablegroup">
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th>Select</th>
-                        <th>Picture</th>
-                        <th>Breed</th>
-                        <th>Name</th>
-                        <th>Age</th>
-                        <th>Condition</th>
-                        <th>Gender</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
+                        </tr>
+                            </thead>
+                            <tbody>
+                    {allPets.map((item) => (
+                        <tr key={item.petId}>
                         <td><input type="checkbox"/></td>
                         <td><img className="photos" src="/photos/golden.jpg" alt="Golden Retriever"/></td>
-                        <td>Golden Retriever</td>
-                        <td>Buddy</td>
-                        <td>3</td>
-                        <td>Good</td>
-                        <td>F</td>
-
+                        <td>{item.breed}</td>
+                        <td>{item.name}</td>
+                        <td>{item.age}</td>
+                        <td>{item.petCondition}</td>
+                        <td>{item.gender}</td>
+                        <td>{item.status}</td>
                     </tr>
-                    <tr>
-                        <td><input type="checkbox"/></td>
-                        <td><img className="photos" src="/photos/persian-cat-.webp" alt="Persian Cat" /></td>
-                        <td>Persian Cat</td>
-                        <td>Whiskers</td>
-                        <td>2</td>
-                        <td>Good</td>
-                        <td>M</td>
-
-                    </tr>
-                    <tr>
-                        <td><input type="checkbox"/></td>
-                        <td><img className="photos" src="/photos/maccao.webp" alt="Parrot" /></td>
-                        <td>Parrot</td>
-                        <td>Polly</td>
-                        <td>1</td>
-                        <td>Good</td>
-                        <td>M</td>
-
-                    </tr>
-                </tbody>
-            </table>
-        </section> */}
-    </Container>
-    
-    </>
+                    ))}
+                        </tbody>
+                    </Table>
+                </div>
+        </Container>
+    </div>
   )
 }
 
