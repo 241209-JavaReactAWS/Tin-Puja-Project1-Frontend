f436d9c323042e1b41c482370d92eede3455ba67
pujakunche <kunchepuja1994@gmail.com>
2024-12-22 01:16:47 -0500

    added more pet images & added pets to homepage

--- pet-adoption-app/src/components/user/home/content/HomeContent.tsx
+++ pet-adoption-app/src/components/user/home/content/HomeContent.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import "../userHome.css"
 import { Col, Container, Row } from 'react-bootstrap';
+import flower_dog from "../../../../img/flower-dog.jpg";
+import aussie_puppy from "../../../../img/aussiepuppy.jpg";
+import cat from "../../../../img/cat.jpg";
 
 function UserHomeContent() {
 
@@ -10,8 +13,16 @@ function UserHomeContent() {
         <section className="midsectiongroup">
             <h2 className="midsection"> Adopt a Life today and save a Life</h2>
             <p className="midsection">Adopting a pets will bring happiness and joy in your Life. And we make adoption process easy.</p>
-
         </section>
+
+        <div className='contentimggroup'>
+          <Row>
+            <Col><img className='contentimg' src={aussie_puppy} alt="picture of an aussie puppy" /></Col>
+            <Col><img className='contentimg' src={cat} alt="picture of cat on their backlegs" /></Col>
+            <Col><img className='contentimg' src={flower_dog} alt="a goldendoodle in a flower background" /></Col>
+          </Row>
+        </div>
+
         <div className="=buttoncontainer">
           <Row>
             <Col lg={4}></Col> 
